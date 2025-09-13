4a5e3cf66fe55ba884951ccb37afc81b62c80688
AbdulAhadSiddiqui-0786 <abdulahadsiddiqui23@gmail.com>
2025-09-14 00:24:00 +0530

    first commit

--- backend/elevenlabs_backend/audio_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import AudioLanguage
-
-class AudioLanguageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AudioLanguage
-        fields = ['code', 'name', 'flag_code', 'audio_url']
